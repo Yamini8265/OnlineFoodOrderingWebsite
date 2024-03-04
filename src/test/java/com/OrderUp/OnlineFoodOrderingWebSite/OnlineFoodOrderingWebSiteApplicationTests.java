7f79e4d8977a74eaeb37bdd37cedbd6d8980dd0b
Yamini <saraswati.yamu@gmail.com>
2024-03-04 11:52:36 +0530

    First Commit

---
+++ src/test/java/com/OrderUp/OnlineFoodOrderingWebSite/OnlineFoodOrderingWebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OrderUp.OnlineFoodOrderingWebSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodOrderingWebSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
